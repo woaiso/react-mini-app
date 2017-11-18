bec990c86a09f3388ead2030859887cc2518dc6f
廖云富 <yunful@jumei.com>
2017-11-18 23:53:29 +0800

    初始化配置

---
+++ dist/pages/user.jsx
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const React = require("react");
+class User extends React.Component {
+    render() {
+        return <div />;
+    }
+}
---
+++ src/pages/user.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+class User extends React.Component<any, any> {
+  render(){
+    return <div className="jack"/>;
+  }
+}
